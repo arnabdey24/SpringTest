635cc060fa7ff2dde8c10a68b5c4e753a6736752
Arnab Dey <arnabdey@Arnabs-Laptop.local>
2023-06-19 10:29:36 +0600

    .

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,48 @@
+package com.example.demo.student;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
